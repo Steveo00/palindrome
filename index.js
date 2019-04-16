c6a67fbaf41ef01fa0a0e701c96ced6355195e5e
Steveo00 <srwillis@ozemail.com.au>
2019-04-16 14:33:56 +1000

    finish working and refactoring palindrome method.

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+module.exports = Phrase;
+
 // Reverses a string
 function reverse(string) {
   return Array.from(string).reverse().join("");
@@ -9,7 +11,15 @@ function Phrase(content) {
 
   // Returns content processed for palidrome testing.
   this.processedContent = function processedContent() {
-    return this.content.toLowerCase();
+    return this.letters().toLowerCase();
+  }
+
+  // Returns the letters in the content.
+  // For example:
+  //   new Phrase("Hello, world!").letters() === "Helloworld"
+  this.letters = function letters() {
+    let theLetters = [];
+    return Array.from(this.content).filter(c => c.match(/[a-z]/i)).join("");
   }
 
   // Returns true if the phrase is a palindrome, false otherwise;
